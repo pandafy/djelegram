b905b54257568a91100ab7540653c265cb1d5e78
TheOneAboveAllTitan <the.one.above.all.titan@gmail.com>
2020-05-01 03:24:43 +0530

    Send message, last message, logout
    - Send message is working
    - Person name of last message is shown on chat card
    - Added logout button

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login', views.login, name="login"),
     path('chat', views.chat, name="chat"),
-
     path('chat/<id>', views.chat, name="chat"),
+    path('send-message', views.send_messages, name='send-message'),
+    path('logout', views.logout, name="logout"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -7,12 +7,15 @@ from .utils import waiting_for_response, send_code_request
 import time 
 import json
 from django.contrib import messages
+from datetime import datetime
+from home.custom_filters import get_item
 # from telethon.sync import TelegramClient
 
 
 client =  TelegramClient('asd',settings.API_ID, settings.API_HASH)
 client.loop.run_until_complete(client.connect())
 loop = asyncio.get_event_loop()
+entities = dict()
 
 # Create your views here.
 
@@ -53,6 +56,7 @@ def login(request):
             try:
                 messages.success(request,"Authentication token has been verified, you will be redirected in a jiffy")
                 request.session['user'] = response.to_json()
+                request.session['user_id'] = response.id
                 return redirect('chat')
             except Exception as e:
                 print(e, response)
@@ -63,15 +67,79 @@ def login(request):
     return render(request, 'home/login.html', context=context)
 
 
-def chat(request, id=None):
+def chat(request, id=0):
     """
     Renders and handles chat page
     """
     context = dict()
-
+    if request.session.get('user_id', None) == None:
+        return redirect('login')
     if request.POST:
         pass
-    response = client.loop.run_until_complete(client.get_dialogs())
-    for dialog in response:
-        print(dialog.title, dialog.message.text, str(dialog.date))
+
+    if id:
+        try:
+            entity = client.loop.run_until_complete(client.get_entity(int(id)))
+            messages = client.loop.run_until_complete(client.get_messages(entity, limit=20))
+            context["messages"] = messages
+            request.session['recipient'] = int(id)
+            # Use Celery task for this :P
+            # for message in messages[:-1]:
+            #     client.loop.run_until_complete(message.mark_read())
+        except Exception as e:
+            pprint(e)
+    try:
+
+        response = client.loop.run_until_complete(client.get_dialogs())
+        channels = []
+        for dialog in response:
+            temp = dict(name=dialog.title, 
+                                message = dialog.message.text,
+                                id = dialog.entity.id,
+                                unread_count= dialog.unread_count,
+                                unread_mentions_count= dialog.unread_mentions_count,
+                                entity=dialog.entity,                                
+                                )
+            temp['date'] = dialog.date.strftime('%H:%M') if dialog.date.date() == datetime.now().date() else dialog.date.strftime('%x')
+
+            try:
+                temp['sender'] = str(dialog.message.from_id)
+                entities[temp['sender']]
+            except:
+                try:
+                    entities[temp['sender']] =client.loop.run_until_complete(client.get_entity(dialog.message.from_id)).first_name
+                except:
+                    pass
+            
+
+            try:
+                if int(id) == temp['id']:
+                    context["selected_chat"] = temp
+            except Exception as e:
+                pprint("No chat selected", e)
+            channels.append(temp)
+
+        context['channels'] = channels
+    except Exception as e:
+        pprint("asdasdasd",e)
+    context['entities'] = entities
     return render(request, 'home/chat.html', context=context)
+
+def send_messages(request):
+    if request.POST:
+        recipient = request.POST.get('recipient')
+        text = request.POST.get('text')
+        client.loop.run_until_complete(client.send_message(int(recipient),message=text))
+        return redirect('chat', int(recipient))
+
+def logout(request):
+    temp = False
+    while not temp:
+        temp = client.loop.run_until_complete(client.log_out())
+        del request.session['phone_code_hash']
+        del request.session['user']
+        del request.session['user_id']
+        del request.session['recipient']
+    client.loop.run_until_complete(client.connect())
+
+    return redirect('login')
